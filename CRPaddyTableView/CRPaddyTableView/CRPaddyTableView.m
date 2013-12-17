b585449d87d91c9bd32cebb8ec6287f32f452aff
Ryan Bertrand <ryan@appbeast.com>
2013-12-16 17:31:02 -0800

    Backward compatibility for < iOS7

--- CRPaddyTableView/CRPaddyTableView/CRPaddyTableView.m
+++ CRPaddyTableView/CRPaddyTableView/CRPaddyTableView.m
@@ -8,6 +8,8 @@
 
 #import "CRPaddyTableView.h"
 
+static BOOL isUIDynamicAnimatorSupported = YES;
+
 @interface CRPaddyTableView(){
     UIDynamicAnimator *_pAnimator;
     UIGravityBehavior *_gBehavior;
@@ -21,43 +23,67 @@
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
-        _pAnimator = [[UIDynamicAnimator alloc] initWithReferenceView:self];
-        [_pAnimator addBehavior:_gBehavior];
+        [self _commonInit];
     }
     return self;
 }
 
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder: aDecoder];
+    if (self) {
+        [self _commonInit];
+    }
+    return self;
+}
+
+-(void)_commonInit{
+    if([UIDynamicAnimator class]){
+        _pAnimator = [[UIDynamicAnimator alloc] initWithReferenceView:self];
+        [_pAnimator addBehavior:_gBehavior];
+    }
+    else{
+        //iOS 6 or less
+        isUIDynamicAnimatorSupported = NO;
+    }
+}
+
 
 - (void)addBehaviorsToCell:(UITableViewCell*)cell{
-    for (UIDynamicBehavior* b in _pAnimator.behaviors) {
-        if ([b isKindOfClass:[UIAttachmentBehavior class]]) {
-            if ([[(UIAttachmentBehavior*)b items] containsObject:cell]){
-                [_pAnimator removeBehavior:b];
+    if(isUIDynamicAnimatorSupported){
+        for (UIDynamicBehavior* b in _pAnimator.behaviors) {
+            if ([b isKindOfClass:[UIAttachmentBehavior class]]) {
+                if ([[(UIAttachmentBehavior*)b items] containsObject:cell]){
+                    [_pAnimator removeBehavior:b];
+                }
             }
         }
+        
+        CGPoint anchorPoint = CGPointMake(cell.bounds.size.width/2, cell.frame.origin.y);
+        
+        UIAttachmentBehavior* attachmentBehavior = [[UIAttachmentBehavior alloc] initWithItem:cell attachedToAnchor:anchorPoint];
+        [attachmentBehavior setLength:0];
+        [attachmentBehavior setFrequency:1];
+        [attachmentBehavior setDamping:0.2];
+        [_pAnimator addBehavior:attachmentBehavior];
     }
-    
-    CGPoint anchorPoint = CGPointMake(cell.bounds.size.width/2, cell.frame.origin.y);
-
-    UIAttachmentBehavior* attachmentBehavior = [[UIAttachmentBehavior alloc] initWithItem:cell attachedToAnchor:anchorPoint];
-    [attachmentBehavior setLength:0];
-    [attachmentBehavior setFrequency:1];
-    [attachmentBehavior setDamping:0.2];
-    [_pAnimator addBehavior:attachmentBehavior];
 }
 
 - (void)configurePoint{
-    for (UIDynamicBehavior* b in _pAnimator.behaviors) {
-        if ([b isKindOfClass:[UIAttachmentBehavior class]]) {
-            CGPoint p = [(UIAttachmentBehavior*)b anchorPoint];
-            p.y += 10;
-            [(UIAttachmentBehavior*)b setAnchorPoint:p];
+    if(isUIDynamicAnimatorSupported){
+        for (UIDynamicBehavior* b in _pAnimator.behaviors) {
+            if ([b isKindOfClass:[UIAttachmentBehavior class]]) {
+                CGPoint p = [(UIAttachmentBehavior*)b anchorPoint];
+                p.y += 10;
+                [(UIAttachmentBehavior*)b setAnchorPoint:p];
+            }
         }
     }
 }
 
 - (void)dealloc{
-    [_pAnimator removeAllBehaviors];
+    if(isUIDynamicAnimatorSupported){
+        [_pAnimator removeAllBehaviors];
+    }
 }
 
 @end
